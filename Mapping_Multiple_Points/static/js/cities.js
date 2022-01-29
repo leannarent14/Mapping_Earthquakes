f4192d145a5801d4995c8db7ff796f61d7e9e86c
leannarent14 <90877777+leannarent14@users.noreply.github.com>
2022-01-28 18:20:53 -0600

    maybe the whole file

---
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -0,0 +1,10 @@
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
+  
+  // Loop through the cities array and create one marker for each city.
+  cities.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+  });
